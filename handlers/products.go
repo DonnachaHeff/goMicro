249daf7db5c48263b23d1d9cbd80ad30f3b69f3b
Donnacha Heffernan <Donnacha.Heffernan@bd.com>
2021-11-16 09:25:49 +0000

    Adding Products
    Curl not working as expected. Might be windows machine issue. Check on
    Linux before merge.

--- handlers/products.go
+++ handlers/products.go
@@ -50,5 +50,5 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request){
 		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 	}
 
-	p.l.Printf("Prod %#v", prod)
+	data.AddProduct(prod)
 }
