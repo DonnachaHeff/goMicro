853904d81081f3faf1ddd34130ce232f680b8601
Donnacha Heffernan <Donnacha.Heffernan@bd.com>
2021-11-22 10:53:01 +0000

    Moving from std to gorilla framework

--- main.go
+++ main.go
@@ -9,14 +9,27 @@ import (
 	"time"
 
 	"github.com/DonnachaHeff/goMicro/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	// Gorilla mux
+	sm := mux.NewRouter()
+
+	// sub routes
+
+	// get subrouters
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	// put subrouters
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+
+	// sm.Handle("/products", ph)
 
 	s := &http.Server{
 		Addr: ":9090",
